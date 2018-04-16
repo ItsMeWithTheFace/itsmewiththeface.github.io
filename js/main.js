3a9663a056cbbe395f934ba8802df7d5f4a7335c
rakinuddin <rakin.uddin@mail.utoronto.ca>
2018-04-15 21:17:11 -0400

    js: added vertical navbar, color invertion

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
 $(document).ready(function() {
     $('#fullpage').fullpage({
         scrollingSpeed: 900,
+        navigation: true,
+
+        onLeave: function(index, nextIndex, direction) {
+            // if we're on the white pages (top and bottom panels)
+            // then change colors
+            if ([1,7].indexOf(nextIndex) == -1) {
+                $('#fp-nav').addClass('invert');
+            }
+            else{
+                $('#fp-nav').removeClass('invert');
+            }    
+        },
     });
 });
